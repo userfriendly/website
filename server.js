110b97b8effe90dbfab0c162ed409abc44fb8fa7
Moritz Kraft <userfriendly@web.de>
2022-11-25 03:00:37 +0000

    First changes: Dockerfile, template etc.

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
 
-'use strict';
-
-const express = require('express');
+// 'use strict'; // or don't, it's up to you, man
 
 // Constants
 const PORT = 3000;
 const HOST = '0.0.0.0';
 
+const express = require('express');
+const path = require('path');
+
 // App
 const app = express();
-app.get('/', (req, res) => {
-	res.send('Hello remote world!\n');
+app.use(express.json());
+app.use(express.static("express"));
+
+// Default URL for website
+app.use('/', function(req,res){
+  res.sendFile(path.join(__dirname+'/express/index.html')); // __dirname will resolve to project folder.
 });
 
+// Listen for requests
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
